e92b4221cfb291665a2a58faafad50ef7adcd429
Mr-Wusu <prince.agboinou_wusu@yahoo.com>
2025-12-17 00:51:35 +0100

    Fixing path issue

---
+++ app/_components/Navigation/ProfileOpen.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import Link from "next/link";
+import {
+  TiCloudStorageOutline,
+  TiCogOutline,
+  TiShoppingCart,
+  TiTimes,
+} from "react-icons/ti";
+import { GiThink } from "react-icons/gi";
+
+// Define the props interface to match useUser() return type
+interface ProfileOpenProps {
+  setIsProfileOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  user: {
+    firstName?: string;
+    surname?: string;
+    email?: string;
+    isAdmin: boolean;
+  };
+  btn: string;
+  nameFont: string;
+}
+
+export default function ProfileOpen({
+  setIsProfileOpen,
+  user,
+  btn,
+  nameFont,
+}: ProfileOpenProps) {
+  function toggleModal(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+    setIsProfileOpen(false);
+  }
+
+  return (
+    <div className="w-fit absolute right-3 mt-5 bg-white shadow-lg rounded-[.5rem] p-4 z-50 flex flex-col items-start gap-3 profile-open">
+      <div
+        className="h-6 w-6 rounded-full bg-rose-500 absolute top-[-.795rem] right-[-.8rem] cursor-pointer grid place-content-center"
+        onClick={toggleModal}
+      >
+        <TiTimes className="text-white" />
+      </div>
+
+      <div className="text-darkRose2 text-nowrap">
+        <p className={`${nameFont} md+:text-base`}>
+          {user.firstName} {user.surname}
+          <span className="text-rose-900 ml-1 text-sm">
+            {user.isAdmin ? "(Admin)" : ""}
+          </span>
+        </p>
+        <p className="text-sm">{user.email}</p>
+      </div>
+      <div className="w-full h-[2px] bg-darkRose2" />
+      <div className="">
+        <ul className="flex flex-col gap-3 text-darkRose2 py-1 px-1 text-base">
+          {!user.isAdmin && (
+            <li className="flex items-center gap-2 hover:text-darkRose1 px-1 hover:scale-110 transition-all duration-300 rounded-[.4rem]">
+              <TiShoppingCart />
+              <Link
+                className={`${nameFont} text-[0.89rem]`}
+                href="/cart"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsProfileOpen(false);
+                }}
+                scroll={false}
+              >
+                Cart
+              </Link>
+            </li>
+          )}
+
+          {!user.isAdmin && (
+            <>
+              <li className="flex items-center gap-2 hover:text-darkRose1 px-1 hover:scale-110 transition-all duration-300 rounded-[.4rem]">
+                <TiCogOutline />
+                <Link
+                  className={`${nameFont} text-[0.89rem]`}
+                  href="/users/settings"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsProfileOpen(false);
+                  }}
+                  scroll={false}
+                >
+                  Settings
+                </Link>
+              </li>
+            </>
+          )}
+          {user.isAdmin && (
+            <>
+              <li className="flex items-center gap-2 hover:text-darkRose1 px-1 hover:scale-110 transition-all duration-300 rounded-[.4rem]">
+                <TiCloudStorageOutline />
+                <Link
+                  className={`${nameFont} text-[0.89rem]`}
+                  href="/dashboard/upload-cloth"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsProfileOpen(false);
+                  }}
+                  scroll={false} // Prevents scrolling to the top of the page
+                >
+                  Upload Cloth
+                </Link>
+              </li>
+              <li className="flex items-center gap-2 hover:text-darkRose1 px-1 hover:scale-110 transition-all duration-300 rounded-[.4rem]">
+                <TiCogOutline />
+                <Link
+                  className={`${nameFont} text-[0.89rem]`}
+                  href="/dashboard/cloth-orders"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsProfileOpen(false);
+                  }}
+                  scroll={false}
+                >
+                  Cloth Orders
+                </Link>
+              </li>
+              <li className="flex items-center gap-2 hover:text-darkRose1 px-1 hover:scale-110 transition-all duration-300 rounded-[.4rem]">
+                <GiThink className="text-rose-900 ml-[0.155rem]" />
+                <Link
+                  className={`${nameFont} text-[0.89rem]`}
+                  href="/dashboard/customer-suggestions"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsProfileOpen(false);
+                  }}
+                  scroll={false}
+                >
+                  Suggestions
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      <div className="w-full h-[2px] bg-darkRose2" />
+
+      <div
+        className={`${btn} h-fit bg-gradient-to-r from-rose-700 to-rose-400 hover:bg-gradient-to-r hover:from-rose-600 hover:to-rose-300 active:scale-95 w-[100px] text-lightRose1 py-1 tracking-wide rounded self-center transition-bg duration-300 ease-in-out grid place-content-center `}
+      >
+        Sign Out
+      </div>
+    </div>
+  );
+}
---
+++ app/_components/Navigation/SideNav.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { HiOutlineMenu } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
+import {useSelector} from "react-redux"
+import {RootState} from "@/state/store"
+
+export default function SideNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const user = useSelector((state:RootState)=>state.user)
+
+  function toggleSideNav() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className="relative ">
+      <HiOutlineMenu
+        className="text-[1.6rem] sm+:text-[1.575rem] cursor-pointer"
+        onClick={toggleSideNav}
+      />
+      {isOpen && (
+        <>
+          <div
+            className="w-screen h-screen fixed top-0 left-0 bottom-0 right-0 bg-black opacity-30 z-10"
+            onClick={() => setIsOpen(false)}
+          />
+          <div className="absolute w-[11rem] rounded-[.7rem] bg-lightRose1 h-fit flex flex-col pl-1 shadow-sm shadow-black top-[2.2rem] menu-open z-30">
+            <div
+              className="hover:bg-rose-500 transition-all duration-200 ease-in absolute right-[-.8rem] top-[-.5rem] shadow-sm shadow-black/50 bg-rose-600 text-lightRose1 h-7 w-7 grid place-content-center rounded-full"
+              onClick={toggleSideNav}
+            >
+              <IoClose className="cursor-pointer font-bold" />
+            </div>
+            <ul className="flex flex-col gap-[1rem] p-[1rem] text-[1rem] text-darkRose2">
+              <li
+                onClick={toggleSideNav}
+                className="hover:scale-110 transition-all duration-200 ease-out"
+              >
+                <Link href="/">Home</Link>
+              </li>
+              <li
+                onClick={toggleSideNav}
+                className="hover:scale-110 transition-all duration-200 ease-out"
+              >
+                <Link href="/about-us">About Us</Link>
+              </li>
+              <li
+                onClick={toggleSideNav}
+                className="hover:scale-110 transition-all duration-200 ease-out"
+              >
+                <Link href="/contact-us">Contact Us</Link>
+              </li>
+              {user.isLoggedIn && !user.isAdmin && (
+                <li
+                  onClick={toggleSideNav}
+                  className="hover:scale-110 transition-all duration-200 ease-out"
+                >
+                  <Link href="/measurements">Measurements</Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
